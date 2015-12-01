7cb10aab93e232f5296e46ee0040ebdc914b0f03
quirin <quirin.koerner@tum.de>
2015-12-01 18:26:43 +0100

    framework

---
+++ parking/src/main.cpp
@@ -0,0 +1,21 @@
+#include "parking_lib/parking_lib.h"
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "parking_node");
+    parking_lib pl;
+
+    ros::Rate loop_rate(50);
+
+    ROS_INFO("Started Parking Node");
+
+    while(ros::ok())
+    {
+        ROS_INFO("Parking Test");
+        //teleop_joy.joy_communication_publisher.publish(teleop_joy.joy_state);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
