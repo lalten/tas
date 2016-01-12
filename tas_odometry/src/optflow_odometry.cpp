b920e642ccb39d2a9af5f0caf890280e4e23e03e
Laurenz <github@laure.nz>
2016-01-12 15:53:15 +0100

    Solve Problem with TF timing

--- tas_odometry/src/optflow_odometry.cpp
+++ tas_odometry/src/optflow_odometry.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <geometry_msgs/TwistWithCovarianceStamped.h>
 #include <px_comm/OpticalFlowRad.h>
+#include <regex>
 
 ros::Publisher twist_publisher;
 double fix_covariance;
@@ -13,8 +14,12 @@ void flow_callback (const px_comm::OpticalFlowRad::ConstPtr& opt_flow) {
 		return;
 	}
 
+	// Build new message from old header
 	geometry_msgs::TwistWithCovarianceStamped twist;
 	twist.header = opt_flow->header;
+	twist.header.stamp = ros::Time::now(); // Otherwise the timestamp on the message is more than the cache length earlier than the newest data in the transform cache
+	if(twist.header.frame_id.substr(0,1) == "/")
+		twist.header.frame_id = twist.header.frame_id.erase(0,1); // Otherwise: error, because tf2 frame_ids cannot start with a '/'
 
 	// translation from optical flow, in m/s
 	twist.twist.twist.linear.x = (opt_flow->integrated_x/opt_flow->integration_time_us)/opt_flow->distance;
@@ -27,10 +32,10 @@ void flow_callback (const px_comm::OpticalFlowRad::ConstPtr& opt_flow) {
 	twist.twist.twist.angular.z = opt_flow->integrated_zgyro/opt_flow->integration_time_us;
 
 	// Populate covariance matrix with uncertainty values
-  double uncertainty = fix_covariance;
-  if (fix_covariance == 0) {
+	double uncertainty = fix_covariance;
+	if (fix_covariance == 0) {
 	  uncertainty = pow(10, -1.0 * opt_flow->quality / (255.0/6.0));
-  }
+	}
 	twist.twist.covariance.assign(0.0); // We say that generally, our data is uncorrelated to each other
 	// However, we have uncertainties for
 	// x, y, z, rotation about X axis, rotation about Y axis, rotation about Z axis
